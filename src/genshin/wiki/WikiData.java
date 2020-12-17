3f5b0e562d73a1127ba603d5a9a5e8d21840f5aa
Brian-Kraosio <briansayudha@gmail.com>
2020-12-17 23:01:52 +0700

    add picture bug fixed

--- src/genshin/wiki/WikiData.java
+++ src/genshin/wiki/WikiData.java
@@ -9,6 +9,7 @@ package genshin.wiki;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
@@ -38,7 +39,11 @@ public class WikiData {
     }
     
     private void setWiki(String keyword) {
-        
+        File image = null;
+        ImageIcon imIco;
+        Image imFit;
+        Image imgFit;
+        String path;
         switch (keyword.toLowerCase()) {
             case "klee":
                 genshin.getTxt_CharArea().setText("Mondstadt");
@@ -51,9 +56,11 @@ public class WikiData {
                 genshin.getTxt_CharArtifact1().setText("Crimson Witch of Flames x2");
                 genshin.getTxt_CharArtifact2().setText("Gladiator's Finale x2");
                 genshin.getTxt_CharArtifact3().setText("Wanderer's Troupe x2 / x4");
-                 ImageIcon imIco = new ImageIcon("E:\\TUGAS\\Jarkom\\Semester 5\\genshin-wiki\\klee.png");
-                Image imFit = imIco.getImage();
-                Image imgFit = imFit.getScaledInstance(genshin.getPicture_label().getWidth(), genshin.getPicture_label().getHeight(), Image.SCALE_SMOOTH);
+                image = new File("klee.png");
+                path = image.getAbsolutePath();
+                imIco = new ImageIcon(path);
+                imFit = imIco.getImage();
+                imgFit = imFit.getScaledInstance(genshin.getPicture_label().getWidth(), genshin.getPicture_label().getHeight(), Image.SCALE_SMOOTH);
                 genshin.getPicture_label().setIcon(new ImageIcon(imgFit));
                 break;
         }
